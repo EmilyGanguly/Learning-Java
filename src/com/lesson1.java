f4c20a6acf2a362a38b5d398069a4115fed72936
Emily Ganguly <ganguly.emily13@gmail.com>
2021-03-14 18:48:32 +0530

    Initial commit

---
+++ src/com/lesson1.java
@@ -0,0 +1,26 @@
+package com;
+
+public class lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String name = "Emily";
+        System.out.println(name);
+        boolean a = true;
+        int x = 7;
+        boolean y = true;
+        float b = 6.67F;
+        boolean isAdult = false;
+
+        System.out.println(x);
+        System.out.println(b);
+        System.out.println(isAdult);
+        double d = 5.678349D;
+        System.out.println(d);
+        char c = 'A';
+        System.out.println(c);
+        int u = 15;
+        int v = 25;
+        System.out.println("The sum of  u  and v is ");
+        System.out.println(u + v);
+    }
+}
