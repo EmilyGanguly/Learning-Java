8d539c686697c65d4cc98a7cfa3589cc2c7e8519
Emily Ganguly <ganguly.emily13@gmail.com>
2021-03-15 09:43:34 +0530

    Initial commit-2

--- src/com/lesson1.java
+++ src/com/lesson1.java
@@ -11,6 +11,7 @@ public class lesson1 {
         float b = 6.67F;
         boolean isAdult = false;
 
+
         System.out.println(x);
         System.out.println(b);
         System.out.println(isAdult);
@@ -18,9 +19,33 @@ public class lesson1 {
         System.out.println(d);
         char c = 'A';
         System.out.println(c);
+
+        //Operators
         int u = 15;
         int v = 25;
         System.out.println("The sum of  u  and v is ");
-        System.out.println(u + v);
+        System.out.print(u + v);
+
+        int q = 60;
+        int p = 54;
+        System.out.println("The sum of p and q is ");
+        System.out.print(p+q);
+
+        System.out.println("The value of p - q is ");
+        System.out.print(p-q);
+
+        System.out.print("The value of p*q is ");
+        System.out.print(p*q);
+
+        System.out.println("The value of p/q is ");
+        System.out.print(p/q);
+
+        System.out.println("The value of p%q is ");
+        System.out.print(p%q);
+
+        System.out.println(p++);
+        System.out.println(p--);
+        System.out.println(++p);
+        System.out.println(--p);
     }
 }
